57a8d1876241d6cb3170ee18b81801cd2ddd8218
nikki-chin <nikkichin119@gmail.com>
2025-09-25 23:39:44 +0800

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://librarymanagementsystem-backend-iea2.onrender.com",
   withCredentials: true, // send cookies (refresh token)
 });
 
